52894651a70005651a724eb548c920602ff334e2
Morenkov <ivanmorenkov@mail.ru>
2018-04-23 02:41:31 +0400

    test2

---
+++ db.sql
@@ -0,0 +1,12 @@
+/*
+-- Query: SELECT * FROM db.products
+LIMIT 0, 1000
+
+-- Date: 2018-04-22 19:17
+*/
+INSERT INTO `products` (`id`,`name`,`count`,`categories`) VALUES (1,'lg l90',1,'phone 3g 2g cheap quick_memo');
+INSERT INTO `products` (`id`,`name`,`count`,`categories`) VALUES (2,'lg k8',10,'phone 4g knock_code android_7');
+INSERT INTO `products` (`id`,`name`,`count`,`categories`) VALUES (3,'samsung GT',12,'tablet 3g 10_inch');
+INSERT INTO `products` (`id`,`name`,`count`,`categories`) VALUES (4,'Nokia c6',1,'phone 3g');
+INSERT INTO `products` (`id`,`name`,`count`,`categories`) VALUES (6,'lenovo a319',0,'phone 3g 1gb');
+INSERT INTO `products` (`id`,`name`,`count`,`categories`) VALUES (7,'iphone X',0,'phone 4g 3gb');
